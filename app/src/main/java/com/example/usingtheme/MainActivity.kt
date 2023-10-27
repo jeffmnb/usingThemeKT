61b0cbf24ffb482a9307c110d75558a3351d5943
Jeferson <jeferson.nborda@gmail.com>
2023-10-26 22:53:40 -0300

    feat: upload project

---
+++ app/src/main/java/com/example/usingtheme/MainActivity.kt
@@ -0,0 +1,68 @@
+package com.example.usingtheme
+
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.widget.Toolbar
+import java.util.prefs.AbstractPreferences
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var sharedPreferences: SharedPreferences
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val toobar: Toolbar = findViewById<Toolbar>(R.id.toolbar)
+
+        setSupportActionBar(toobar)
+
+        sharedPreferences = getSharedPreferences("theme_preferences", Context.MODE_PRIVATE)
+
+        applySavedTheme()
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_theme, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_theme_ligth -> {
+                applyTheme(AppCompatDelegate.MODE_NIGHT_NO)
+                true
+            }
+
+            R.id.menu_theme_night -> {
+                applyTheme(AppCompatDelegate.MODE_NIGHT_YES)
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun applyTheme(themeMode: Int) {
+        AppCompatDelegate.setDefaultNightMode(themeMode)
+        saveTheme(themeMode)
+    }
+
+    private fun saveTheme(themeMode: Int) {
+        val editor = sharedPreferences.edit()
+        editor.putInt("theme_mode", themeMode)
+        editor.apply()
+    }
+
+    private fun applySavedTheme() {
+        val savedThemeMode = sharedPreferences.getInt("them_mode", AppCompatDelegate.MODE_NIGHT_NO)
+        applyTheme(savedThemeMode)
+    }
+}
